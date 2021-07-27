b3facc8d34d159e779711beb40c3313ea3479961
mrwang007s <mrwang007s@outlook.com>
2021-07-27 23:09:21 +0800

    0.可行性研究--vue组件与vue单文件

--- prototype/dist/index.bundle.js
+++ prototype/dist/index.bundle.js
@@ -1,3 +1,5 @@
+import { openBlock, createBlock } from 'vue';
+
 var MyButton = {
   name: 'MyButton',
   data: function () {
@@ -8,13 +10,29 @@ var MyButton = {
   template: '<button v-on:click="count++"> click me {{count}} !</button>'
 };
 
-MyButton.install = app => app.component('MyButton', MyButton); // 组件库
+var script = {
+    name: 'SfcButton'
+
+};
+
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  return (openBlock(), createBlock("button", null, "SfcButton 666"))
+}
+
+script.render = render;
+script.__file = "src/SfcButton.vue";
+
+MyButton.install = app => app.component('MyButton', MyButton);
+
+script.install = app => app.component('SfcButton', script); // 组件库
 
 
 const Element = {
   MyButton,
+  SfcButton: script,
   install: app => {
     app.use(MyButton);
+    app.use(script);
   }
 };
 
---
+++ prototype/src/SfcButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button>SfcButton 666</button>
+</template>
+
+<script>
+export default {
+    name: 'SfcButton'
+
+}
+</script>
+
+<style>
+
+</style>
